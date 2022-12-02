e93f031d64c0d85cfb0f93c5110c3a17cb035eb7
Peter of the Norse <rahmcoff@gmail.org>
2022-12-01 22:35:46 -0700

    ☆☆

--- 02/main.py
+++ 02/main.py
@@ -26,14 +26,39 @@ def single_game_score(them: str, us: str) -> int:
   return score
 
 
+alt_code = {'X': -1, 'Y': 0, 'Z': 1}
+
+def alternate_rules(input) -> int:
+  total = 0
+  for line in input:
+    line = line.strip()
+    them, us = line.split(' ')
+    them = them_code[them]
+    us = alt_code[us]
+
+    score = them + us
+    if score == 0:
+      score = 3
+    if score == 4:
+      score = 1
+
+    total += score
+    total += 3 * (us + 1)
+
+  return total
+
 class TestToday(unittest.TestCase):
+  test_input = ['A Y', 'B X\n', 'C Z']
 
   def test_total(self):
-    test_input = ['A Y', 'B X\n', 'C Z']
-    self.assertEqual(total_score(test_input), 15)
+    self.assertEqual(total_score(self.test_input), 15)
+
+  def test_alt(self):
+    self.assertEqual(alternate_rules(self.test_input), 12)
 
 
 if __name__ == '__main__':
   with open('./input.txt', 'r') as f:
     code = [line for line in f]
   print(total_score(code))
+  print(alternate_rules(code))
